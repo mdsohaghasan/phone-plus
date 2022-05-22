bd0214b43d7ea382a8596c1776d3e60620e20512
Sohag Hasan <mailsohaghasan@gmail.com>
2022-05-22 23:34:17 +0600

    yti

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="App">
 <<<<<<< HEAD
+<<<<<<< HEAD
       <button class="btn btn-primary">Button</button>
       <button class="rounded-full ...">Save Changes</button>
+=======
+      <button class="btn btn-primary rounded-r-lg text-rose-500">Button</button>
+
+>>>>>>> ad738b8c087b5f7b11cc8d39d25da9e314351d64
 =======
       <button class="btn btn-primary rounded-r-lg text-rose-500">Button</button>
 
