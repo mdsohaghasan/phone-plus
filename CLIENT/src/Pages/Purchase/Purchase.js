70cfe14508f88a0f2aa78e5a180d78a6444883c8
Sohag Hasan <mailsohaghasan@gmail.com>
2022-05-23 05:47:30 +0600

    were

--- CLIENT/src/Pages/Purchase/Purchase.js
+++ CLIENT/src/Pages/Purchase/Purchase.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// import Products from '../Home/Products/Products'
+import { useParams } from 'react-router-dom';
+
+
 
 const Purchase = () => {
+    const { id } = useParams();
+    const [Itemes, setItemes] = useState([]);
+    useEffect(() => {
+        const url = `Items.json/${id}`;
+        console.log(url)
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setItemes(data));
+
+    }, []);
+
     return (
-        <div>Purchase</div>
+        <div>
+            <div><h5 class="card-title">Name : {Itemes.name}</h5></div>
+            <label for="PurchaseModal" class="btn modal-button">open modal</label>
+        </div>
+
+        // <Products></Products>
     )
 }
 
