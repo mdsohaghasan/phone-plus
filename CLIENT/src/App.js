e94bb94801deb9cb112581cea51d1d42cf6c53c6
Sohag Hasan <58013666+mdsohaghasan@users.noreply.github.com>
2022-05-22 23:33:12 +0600

    hgfgh

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button class="btn btn-primary rounded-r-lg text-rose-500">Button</button>
+      <button class="btn btn-primary rounded-r-lg ">Button</button>
+      <button class="btn btn-secondary rounded-r-lg ">Button</button>
+      <button class="btn btn-accent rounded-r-lg ">Button</button>
+      <button class="btn btn-neutral rounded-r-lg ">Button</button>
 
     </div>
   );
--- CLIENT/tailwind.config.js
+++ CLIENT/tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#764AF1",
+          secondary: "#F7D716",
+          accent: "#9BA3EB",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
