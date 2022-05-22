af9028ccab35dac45ba268ae7f93149cd892261a
Sohag Hasan <mailsohaghasan@gmail.com>
2022-05-23 00:30:37 +0600

    banner

--- CLIENT/src/Pages/Home/Home.js
+++ CLIENT/src/Pages/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Headers from '../../Components/Headers/Headers'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Headers></Headers>
+        </div>
     )
 }
 
