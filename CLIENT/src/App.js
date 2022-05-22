a989d002450d0357fb7e457ed5c18f4fc63ebe06
Sohag Hasan <mailsohaghasan@gmail.com>
2022-05-23 00:26:09 +0600

    navbar and route set

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Reviews from './Pages/Reviews/Reviews';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Blogs from './Pages/Blogs/Blogs';
+import About from './Pages/About/About';
+import { Route, Routes } from 'react-router-dom';
+import MyAccount from './Pages/MyAccount/MyAccount';
+import SignIn from './Pages/SignIn/SignIn';
+import SignUp from './Pages/SignUp/SignUp';
+import NotFound from './Pages/NotFound/NotFound';
+import Purchase from './Pages/Purchase/Purchase';
 
 function App() {
   return (
     <div className="App">
 
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/purchase" element={<Purchase></Purchase>} />
+        <Route path="review" element={<Reviews></Reviews>} />
+        <Route path="about" element={<About></About>} />
+        <Route path="blog" element={<Blogs></Blogs>} />
+        <Route path="signIn" element={<SignIn></SignIn>} />
+        <Route path="signUp" element={<SignUp></SignUp>} />
+        <Route path="dashboard" element={<Dashboard></Dashboard>} />
+        <Route path="myAccount" element={<MyAccount></MyAccount>} />
+        <Route path="/*" element={<NotFound></NotFound>} />
+      </Routes>
 
 
     </div>
