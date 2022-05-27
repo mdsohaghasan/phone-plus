67758a0c9e8f2ff57b87f0566001fad2a2a0ca34
Sohag Hasan <58013666+mdsohaghasan@users.noreply.github.com>
2022-05-27 22:16:38 +0600

    jhhj

--- SERVER/index.js
+++ SERVER/index.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
+
 // MONGODB CONNECTION
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pajtj.mongodb.net/?retryWrites=true&w=majority`;
